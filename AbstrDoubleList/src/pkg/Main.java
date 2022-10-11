d9aa335bc24c8daae7c5619dd651edacb03c6103
elphxd <boura.frantisek01@gmail.com>
2022-10-11 22:32:56 +0200

    Pripraveno na konzultace (snad lmao)

--- AbstrDoubleList/src/pkg/Main.java
+++ AbstrDoubleList/src/pkg/Main.java
@@ -2,15 +2,13 @@ package pkg;
 
 import java.io.FileNotFoundException;
 import java.util.Iterator;
-import abstrdoublelist.AbstrDoubleList;
-import abstrdoublelist.AbstrDoubleListException;
 import procesy.Proces;
 import procesy.VyrobniProces;
 import procesy.VyrobniProcesException;
 
 public class Main {
 
-    public static void main(String[] args) throws AbstrDoubleListException {
+    public static void main(String[] args) {
         
         VyrobniProces importovane = new VyrobniProces();
         VyrobniProces generovane = new VyrobniProces();
